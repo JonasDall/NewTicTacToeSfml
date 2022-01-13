11b0fbcec787746e07b4a4d38e7d9c292afedce6
JonasDall <jonaskd@live.no>
2022-01-13 22:23:23 +0100

    Set up Tile class, imported images

--- TicTacToeSfml/main.cpp
+++ TicTacToeSfml/main.cpp
@@ -1,10 +1,73 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+
+class Tile
+{
+	sf::Sprite m_sprite;
+
+public:
+	Tile(sf::Texture *texture, sf::Vector2f position)
+	{
+		m_sprite.setTexture(*texture);
+		m_sprite.setPosition(position);
+	}
+
+	Tile()
+	{
+	}
+
+	void setTexture(sf::Texture texture)
+	{
+		m_sprite.setTexture(texture);
+	}
+
+	sf::Sprite getSprite()
+	{
+		return m_sprite;
+	}
+};
 
 int main()
 {
+	const int textureSize{ 24 };
+	const int boardSize{ 3 };
+
 	sf::RenderWindow window(sf::VideoMode(300, 300), "Uhm");
-	sf::CircleShape circle(150.f);
+
+	int error[3]{};
+
+	sf::Texture tileTexture;
+	error[0] = tileTexture.loadFromFile("Resources/Tile.png");
+	sf::Texture circleTexture;
+	error[1] = circleTexture.loadFromFile("Resources/Circle.png");
+	sf::Texture crossTexture;
+	error[2] = crossTexture.loadFromFile("Resources/Cross.png");
+
+	for (int i{}; i < *std::end(error); ++i)
+	{
+		if (error[i] == -1)
+		{
+			std::cout << "Image " << i << " not loaded";
+			return -1;
+		}
+	}
+	std::cout << "Textures loaded successfully\n";
+
+	std::vector<Tile> tileVector;
+
+	for (unsigned int x{}; x < boardSize; ++x)
+	{
+		for (unsigned int y{}; y < boardSize; ++y)
+		{
+			tileVector.push_back(Tile(&tileTexture, sf::Vector2f(static_cast<float>(textureSize) * x, static_cast<float>(textureSize) * y)));
+		}
+	}
+
+	sf::Sprite spriteTest;
+	spriteTest.setTexture(tileTexture);
 
 	while (window.isOpen())
 	{
@@ -17,7 +80,14 @@ int main()
 			}
 		}
 		window.clear();
-		window.draw(circle);
+
+		for (unsigned int i{}; i < tileVector.size(); ++i)
+		{
+			window.draw(tileVector[i].getSprite());
+		}
+
+		window.draw(spriteTest);
+
 		window.display();
 	}
 
